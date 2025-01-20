6c8e266974ea534531ddc0d66553afccc513a5b3
KeshavRajSharma <jdkeshav01@gmail.com>
2025-01-20 12:44:34 +0545

    Drawn feature added

--- src/components/Calculatewinner.jsx
+++ src/components/Calculatewinner.jsx
@@ -13,12 +13,20 @@ const Calculatewinner = ({ squares }) => {
   ];
   let k = 0;
   let j = 0;
+  let count = 0;
   for (k; k < lines.length; k++) {
     const [a, b, c] = lines[k];
-    // console.log(a, b, c);
-    // console.log(k);
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return <div>Winner is :{squares[c]}</div>;
+      return <div>Winner</div>;
+    } else {
+      for (j; j < 9; j++) {
+        if (squares[j] === null) {
+          count++;
+        }
+      }
+      if (count === 0) {
+        return <div>Drawn</div>;
+      }
     }
   }
 };
